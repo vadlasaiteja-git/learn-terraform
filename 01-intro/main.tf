27edb3529e8e3ffff994093e7848cb4ee6e121e2
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-24 12:13:00 +0530

    Terraform Variables.

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -10,12 +10,23 @@ variable "x" {
   default=10
 }
 
+#List Variable
+variable "y"{
+  default =[1,"sai",0.007,false]
+}
+#List can have a combination of types also
+
 #Print Variable Output
 output "x" {
   value = var.x
 }
 
-# if our value is just variable then var.x , if it is combination of strings then "${var.x}"
+# If our value is just variable then var.x , if it is combination of strings then "${var.x}"
 output "x1" {
   value = "Value of -x ${var.x}"
+}
+
+#Accessing list variable
+output "y1" {
+  value = var.y[0]
 }
