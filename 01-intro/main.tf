b76d2ced06110e43382d159d3969e69b697712ef
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-24 12:03:43 +0530

    Terraform Variables.

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -4,3 +4,18 @@ resource "local_file" "foo" {
   content  = "Hello World"
   filename = "/tmp/foo.bar"
 }
+
+#Normal Variable
+variable "x" {
+  default=10
+}
+
+#Print Variable Output
+output "x" {
+  value = var.x
+}
+
+# if our value is just variable then var.x , if it is combination of strings then "${var.x}"
+output "x1" {
+  value = "Value of -x ${var.x}"
+}
